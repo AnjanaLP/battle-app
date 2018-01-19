002d6223038782c85e8997c33b81dc3d27db7e19
Anjana <anjana.lpn@gmail.com>
2018-01-19 13:42:00 +0000

    extract logic to model

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra/base'
+require 'player'
 
  class Battle < Sinatra::Base
 
-   enable :sessions
+   # enable :sessions
 
    # get '/' do
    #   'Testing infrastructure working!'
@@ -13,20 +14,20 @@ require 'sinatra/base'
    end
 
    post '/names' do
-     session[:player_1_name] = params[:player_1_name]
-     session[:player_2_name] = params[:player_2_name]
+     $player_1_name = params[:player_1_name]
+     $player_2_name = params[:player_2_name]
      redirect '/play'
    end
 
    get '/play' do
-     @player_1_name = session[:player_1_name]
-     @player_2_name = session[:player_2_name]
+     @player_1_name = $player_1_name
+     @player_2_name = $player_2_name
      erb :play
    end
 
    get '/attack' do
-     @player_1_name = session[:player_1_name]
-     @player_2_name = session[:player_2_name]
+     @player_1_name = $player_1_name
+     @player_2_name = $player_2_name
      erb :attack
    end
 
