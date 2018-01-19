6fe7d42dce9485d51d0403170894e031b5e35188
Anjana <anjana.lpn@gmail.com>
2018-01-19 10:50:10 +0000

    extract player class

--- app.rb
+++ app.rb
@@ -1,35 +1,35 @@
 require 'sinatra/base'
 
-class Battle < Sinatra::Base
+ class Battle < Sinatra::Base
 
-  # enable :sessions
+   enable :sessions
 
-  # get '/' do
-  #   'Testing infrastructure working!'
-  # end
+   # get '/' do
+   #   'Testing infrastructure working!'
+   # end
 
-  get '/' do
-    erb :index
+   get '/' do
+     erb :index
+   end
+
+   post '/names' do
+     session[:player_1_name] = params[:player_1_name]
+     session[:player_2_name] = params[:player_2_name]
+     redirect '/play'
+   end
+
+   get '/play' do
+     @player_1_name = session[:player_1_name]
+     @player_2_name = session[:player_2_name]
+     erb :play
+   end
+
+   get '/attack' do
+     @player_1_name = session[:player_1_name]
+     @player_2_name = session[:player_2_name]
+     erb :attack
+   end
+
+    # start the server if ruby file executed directly
+   run! if app_file == $0
   end
-
-  post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    redirect '/play'
-  end
-
-  get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb :play
-  end
-
-  get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb :attack
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,5 @@
 require 'player'
 
 describe Player do
-  subject(:alice) { Player.new('Alice') }
 
-  describe '#name' do
-    it 'returns the name' do
-      expect(alice.name).to eq 'Alice'
-    end
-  end
 end
